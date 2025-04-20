6dc82f199eccfb84dee3e9460ca582bf3f900d17
Harsh231075 <harshsingh231075@gmail.com>
2025-04-20 11:10:00 +0530

    commit 4

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { Navigate, Outlet } from 'react-router-dom';
 const ProtectedRoute = () => {
   const token = localStorage.getItem('token');
 
-  return token ? <Outlet /> : <Navigate to="/login" />;
+  return token ? <Outlet /> : <Navigate to="/home" />;
 };
 
 export default ProtectedRoute;
